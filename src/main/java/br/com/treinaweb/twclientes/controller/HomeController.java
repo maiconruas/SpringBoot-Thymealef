5b4efe7cd4bf7f4819107a27a9be9fdf2909c8f4
Maicon Ruas <57970450+maiconruas@users.noreply.github.com>
2021-06-03 14:08:12 -0300

    first commit

---
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -0,0 +1,54 @@
+package br.com.treinaweb.twclientes.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(ModelMap model) {
+        model.addAttribute("boasVindas", "Bem vindo ao curso de Spring com Thymeleaf");
+
+        List<String> aulas = new ArrayList<>();
+        aulas.add("Aula 01 - Introdução");
+        aulas.add("Aula 02 - Template engines");
+        aulas.add("Aula 03 - Arquivos estáticos");
+        aulas.add("Aula 04 - Exibindo informações");
+
+        model.addAttribute("aulas", aulas);
+
+        return "home";
+    }
+
+    @GetMapping("/mensagem")
+    public ModelAndView mensagem() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Outra mensagem vinda do servidor");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/saudacao")
+    public ModelAndView saudacao(
+        @RequestParam(required = false, defaultValue = "TreinaWeb") String nome
+        ) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+
+        return modelAndView;
+    }
+    
+}
